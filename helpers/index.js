8cd885d4f08a285ba1d1b629628c7a6ce9a5f5ed
Gerson Martinez <gerson_mtzvilla@hotmail.com>
2022-12-11 22:09:58 -0600

    first commit

---
+++ helpers/index.js
@@ -0,0 +1,34 @@
+const formatearDinero = valor => {
+    const formatter = new Intl.NumberFormat('en-US',{
+        style: 'currency',
+        currency: 'USD'
+    })
+    
+    return formatter.format(valor)
+}
+
+const calcularDinero = (cantidad,plazo) => {
+   let total;
+   //Mientras mayor es la cantidad solicitada mayor es el interes
+   if (cantidad < 500) {
+       total = cantidad * 1.5
+   }else if  (cantidad >= 5000 && cantidad < 10000){
+       total = cantidad * 1.4
+   }else if (cantidad >= 10000 && cantidad < 15000){
+       total = cantidad * 1.3
+   }else { total = cantidad * 1.2 }
+   
+   // si el plazo es mayor tambien el interés
+   if (plazo ===6) {
+       total *= 1.1
+   }else if(plazo ===12){
+       total *= 1.2
+   }else{ total*= 1.3}
+
+   return total
+}
+
+export {
+    formatearDinero,
+    calcularDinero
+}
